927f732b8ce50cf6326f9363bb26573d71ed4c0a
Brown, Bryan <bryjbrown@gmail.com>
2015-01-12 14:44:46 -0500

    updated bepress2fo.xsl into new layouts

---
+++ metadata2coverpage_trial/pager.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+INPUT=testinput.xml
+OUTPUT=testoutput.pdf
+
+java -jar saxon9he.jar -o:testintermediate.fo $INPUT bepress2fo-original.xsl
+fop-1.1/fop testintermediate.fo $OUTPUT
