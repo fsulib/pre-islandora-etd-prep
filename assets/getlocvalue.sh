ebfc6d12fb07056beda50507dd35e17e5f6a4a1f
Bryan Brown <bryjbrown@gmail.com>
2015-02-07 14:32:32 -0500

    Fixed getlocvalue.sh

--- assets/getlocvalue.sh
+++ assets/getlocvalue.sh
@@ -1,13 +1,10 @@
 #!/bin/sh
 
-# Usage: ./getlocvalue.sh type/URI
-# where 'type' is either 'names' for name authorities 
-# or 'subjects' for subject authorities
-# and 'URI' is the individual URI ID.
+# Usage: ./getlocvalue.sh URI
 # For example, the authority URI for Diane W. Hodgins is http://id.loc.gov/authorities/names/no2007085308
 # If you already have this URI and want to know what it goes to, run the script as:
-# ./getlocvalue.sh names/no2007085308
+# ./getlocvalue.sh http://id.loc.gov/authorities/names/no2007085308
 # to get the output:
 # Hodgins, Diane W. (Diane Weaver)
 
-curl -sI http://id.loc.gov/authorities/$1 | grep "X-PrefLabel: " | sed 's/X-PrefLabel:\ //'
+curl -sI $1 | grep "X-PrefLabel: " | sed 's/X-PrefLabel:\ //'
