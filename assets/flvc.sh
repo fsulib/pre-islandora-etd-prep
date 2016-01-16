a79de2d6f107e496f5aaf2bc0fc967d382c693c4
mrmiguez <r.m.miguez@gmail.com>
2016-01-16 14:57:45 -0500

    removing program calls by alias

--- assets/flvc.sh
+++ assets/flvc.sh
@@ -4,15 +4,15 @@
 # $1 is target record w/ full path 
 # $2 is batch ID
 
-xml ed -L -i //_:mods -t attr -n xmlns:flvc -v info:flvc/manifest/v1 $1
-if [[ ! -n $( xml el $1 | grep "extension" ) ]]; then
-  xml ed -L -s //_:mods -t elem -n extension $1
+xmlstarlet ed -L -i //_:mods -t attr -n xmlns:flvc -v info:flvc/manifest/v1 $1
+if [[ ! -n $( xmlstarlet el $1 | grep "extension" ) ]]; then
+  xmlstarlet ed -L -s //_:mods -t elem -n extension $1
 fi
-xml ed -L -s //_:extension -t elem -n flvc:flvc $1
-xml ed -L -s //flvc:flvc -t elem -n flvc:owningInstitution -v FSU $1
-xml ed -L -s //flvc:flvc -t elem -n flvc:submittingInstitution -v FSU $1
+xmlstarlet ed -L -s //_:extension -t elem -n flvc:flvc $1
+xmlstarlet ed -L -s //flvc:flvc -t elem -n flvc:owningInstitution -v FSU $1
+xmlstarlet ed -L -s //flvc:flvc -t elem -n flvc:submittingInstitution -v FSU $1
 
 # Add IID identifier
 DID=`basename ${1} | sed "s/.xml//"`
-xml ed -L -s //_:mods -t elem -n identifier -v FSU_${2}_${DID} $1
+xmlstarlet ed -L -s //_:mods -t elem -n identifier -v FSU_${2}_${DID} $1
 perl -pi -e 's/<identifier>/<identifier type="IID">/g' $1
--- assets/namespaces.sh
+++ assets/namespaces.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
 
-xml ed -L -i //mods -t attr -n xmlns -v http://www.loc.gov/mods/v3 $1
-xml ed -L -i //_:mods -t attr -n xmlns:xsi -v http://www.w3.org/2001/XMLSchema-instance $1
-xml ed -L -i //_:mods -t attr -n xmlns:xlink -v http://www.w3.org/1999/xlink $1
-xml ed -L -i //_:mods -t attr -n xmlns:mods -v http://www.loc.gov/mods/v3 $1
-xml ed -L -i //_:mods -t attr -n xmlns:dcterms -v http://purl.org/dc/terms/ $1
-xml ed -L -i //_:mods -t attr -n xmlns:etd -v http://www.ndltd.org/standards/metadata/etdms/1.0/ $1
+xmlstarlet ed -L -i //mods -t attr -n xmlns -v http://www.loc.gov/mods/v3 $1
+xmlstarlet ed -L -i //_:mods -t attr -n xmlns:xsi -v http://www.w3.org/2001/XMLSchema-instance $1
+xmlstarlet ed -L -i //_:mods -t attr -n xmlns:xlink -v http://www.w3.org/1999/xlink $1
+xmlstarlet ed -L -i //_:mods -t attr -n xmlns:mods -v http://www.loc.gov/mods/v3 $1
+xmlstarlet ed -L -i //_:mods -t attr -n xmlns:dcterms -v http://purl.org/dc/terms/ $1
+xmlstarlet ed -L -i //_:mods -t attr -n xmlns:etd -v http://www.ndltd.org/standards/metadata/etdms/1.0/ $1
 
 # fix weird space between URLs in line below
-xml ed -L -i //_:mods -t attr -n xsi:schemaLocation -v "http://www.loc.gov/mods/v3 http://www.loc.gov/standards/mods/v3/mods-3-4.xsd" $1
+xmlstarlet ed -L -i //_:mods -t attr -n xsi:schemaLocation -v "http://www.loc.gov/mods/v3 http://www.loc.gov/standards/mods/v3/mods-3-4.xsd" $1
 
-xml ed -L -i //_:mods -t attr -n version -v 3.4 $1
+xmlstarlet ed -L -i //_:mods -t attr -n version -v 3.4 $1
 
 perl -pi -e 's/<mods/<mods:mods/g' $1
 perl -pi -e 's/<\/mods>/<\/mods:mods>/g' $1
